06395956b36973c920474e14ec94921fb901227a
arrayman1432 <47229155+arrayman1432@users.noreply.github.com>
2019-12-07 14:54:41 +0600

    Updates

---
+++ precache-manifest.2f0f70972001c1035cd32f5ced3d7cf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934a46c9c56a42d886100077f0ad1b08",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "465ba2b875c06462cf8f",
+    "url": "/recipe-app/static/css/main.36bb7df3.chunk.css"
+  },
+  {
+    "revision": "030951666d8df42be715",
+    "url": "/recipe-app/static/js/2.1496fd20.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe-app/static/js/2.1496fd20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "465ba2b875c06462cf8f",
+    "url": "/recipe-app/static/js/main.8a6ef813.chunk.js"
+  },
+  {
+    "revision": "c9e78abdeae374382543",
+    "url": "/recipe-app/static/js/runtime-main.c7667b32.js"
+  }
+]);
